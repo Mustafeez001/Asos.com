b0cb628a519911ebdc6a7f776169d7413cb0d531
Anup <dasanup1999@gmail.com>
2022-09-29 01:07:40 +0530

    update

--- components/navbar.js
+++ components/navbar.js
@@ -4,11 +4,12 @@ function navbar(){
     return ` <div id="nav1">
     <div>
         <div> <a href="index.html">  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ_ORaNu1Id6gnw73nLdy2Fnsifg0aXwQI7sUbB68M&s" alt="">  </a></div>
-    <div><a href="">WOMEN</a></div>
+    <div><a href="/women.html">WOMEN</a></div>
     <div><a href="">MAN</a></div>
     </div>
   <div>
-    <input type="text" name="" id="query" placeholder="Search for items and brands" >
+    <input type="text" name="" id="query" placeholder="Search for items and brands">
+    
   </div>
     <div>
        <a href="/signup.html"> <img src="https://cdn.iconscout.com/icon/premium/png-128-thumb/account-6261620-5141375.png" alt=""></a> 
