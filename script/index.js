a313e23ec91f6feebfe28b9aacb3694ea896d5f7
Anup <dasanup1999@gmail.com>
2022-09-29 00:18:56 +0530

    like file

--- script/index.js
+++ script/index.js
@@ -1,4 +1,7 @@
 import navbar from "../components/navbar.js";
+let nav= document.getElementById("navbar")
+nav.innerHTML=navbar()
 
-let nav=document.getElementById("navbar")
-nav.innerHTML=navbar();
+import footer from "../components/footer.js";
+let foot=document.getElementById("footer");
+foot.innerHTML=footer();
