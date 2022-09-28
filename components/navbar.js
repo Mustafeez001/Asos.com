1f7e26c45f8fd20e93ee8308f4c46c73cd4bf0e3
Abdus Samim <absamim6157@gmail.com>
2022-09-28 13:32:18 +0530

    update

--- components/navbar.js
+++ components/navbar.js
@@ -1,36 +1,36 @@
 
 function navbar(){
 
-    return`
-    <div id="nav1">
+    return ` <div id="nav1">
     <div>
-        <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ_ORaNu1Id6gnw73nLdy2Fnsifg0aXwQI7sUbB68M&s" alt=""></div>
-    <div>WOMAN</div>
-    <div>MAN</div>
+        <div> <a href="index.html">  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ_ORaNu1Id6gnw73nLdy2Fnsifg0aXwQI7sUbB68M&s" alt="">  </a></div>
+    <div><a href="">WOMEN</a></div>
+    <div><a href="">MAN</a></div>
     </div>
   <div>
     <input type="text" name="" id="query" placeholder="Search for items and brands" >
   </div>
     <div>
-        <img src="https://cdn.iconscout.com/icon/premium/png-128-thumb/account-6261620-5141375.png" alt="">
-        <img src="data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2048%2048%22%3E%3Cpath%20fill%3D%22none%22%20d%3D%22M0%200h48v48H0z%22%2F%3E%3Cpath%20d%3D%22M33%206c-3.48%200-6.82%201.62-9%204.17C21.82%207.62%2018.48%206%2015%206%208.83%206%204%2010.83%204%2017c0%207.55%206.8%2013.72%2017.1%2023.07L24%2042.7l2.9-2.63C37.2%2030.72%2044%2024.55%2044%2017c0-6.17-4.83-11-11-11zm-8.79%2031.11-.21.19-.21-.19C14.28%2028.48%208%2022.78%208%2017c0-3.99%203.01-7%207-7%203.08%200%206.08%201.99%207.13%204.72h3.73C26.92%2011.99%2029.92%2010%2033%2010c3.99%200%207%203.01%207%207%200%205.78-6.28%2011.48-15.79%2020.11z%22%2F%3E%3C%2Fsvg%3E" alt="">
-         <img src="https://cdn.iconscout.com/icon/premium/png-128-thumb/bag-2524910-2140655.png" alt="">
-    
+       <a href="/login.html"> <img src="https://cdn.iconscout.com/icon/premium/png-128-thumb/account-6261620-5141375.png" alt=""></a> 
+       <a href="#"> <img src="data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2048%2048%22%3E%3Cpath%20fill%3D%22none%22%20d%3D%22M0%200h48v48H0z%22%2F%3E%3Cpath%20d%3D%22M33%206c-3.48%200-6.82%201.62-9%204.17C21.82%207.62%2018.48%206%2015%206%208.83%206%204%2010.83%204%2017c0%207.55%206.8%2013.72%2017.1%2023.07L24%2042.7l2.9-2.63C37.2%2030.72%2044%2024.55%2044%2017c0-6.17-4.83-11-11-11zm-8.79%2031.11-.21.19-.21-.19C14.28%2028.48%208%2022.78%208%2017c0-3.99%203.01-7%207-7%203.08%200%206.08%201.99%207.13%204.72h3.73C26.92%2011.99%2029.92%2010%2033%2010c3.99%200%207%203.01%207%207%200%205.78-6.28%2011.48-15.79%2020.11z%22%2F%3E%3C%2Fsvg%3E" alt=""></a>
+       <a href="#"><img src="https://cdn.iconscout.com/icon/premium/png-128-thumb/bag-2524910-2140655.png" alt="">
+       </a>
+         
     </div>
 </div>
 <div id="nav2">
-    <div  id="red1">Sale</div>
-    <div>New in</div>
-    <div>Clothing</div>
-    <div>Shoes</div>
-    <div>Shortswear</div>
-    <div>Accessories</div>
-    <div>Trending now</div>
-    <div>Topman</div>
-    <div>Face+Body</div>
-    <div>Brands</div>
-    <div id="red2">Outlet</div>
-    <div>Markplace</div>
+    <div  id="red1"><a href="">Sale</a></div>
+    <div><a href="">New in</a></div>
+    <div><a href="">Clothing</a></div>
+    <div><a href="">Shoes</a></div>
+    <div><a href="">Shortswear</a></div>
+    <div><a href="">Accessories</a></div>
+    <div><a href="">Trending now</a></div>
+    <div><a href="">Topman</a></div>
+    <div><a href="">Face+Body</a></div>
+    <div><a href="">Brands</a></div>
+    <div id="red2"><a href="">Outlet</a></div>
+    <div><a href="">Markplace</a></div>
 </div>
 <div id="nav3">
     <div>
@@ -42,8 +42,9 @@ function navbar(){
     <div>
         <h3>FREE WORLDWIDE DELEVERY</h3>
     </div>
-</div>
+</div>`
     
-    `
+    
+    
 }
 export default navbar;
